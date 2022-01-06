a23c94a9f1835df063f28e901174c46521a9d1af
Ove Doll <moin@ovedoll.de>
2022-01-06 14:15:28 +0100

    fix(pipeline): netlify pipeline

--- packages/site/components/Copy/Copy.stories.tsx
+++ packages/site/components/Copy/Copy.stories.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { HeadlineText } from './index';
+import { Copy } from './index';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Content/HeadlineText',
-  component: HeadlineText,
+  title: 'Content/Copy',
+  component: Copy,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   // argTypes: {
   //   backgroundColor: { control: 'color' },
   // },
-} as ComponentMeta<typeof HeadlineText>;
+} as ComponentMeta<typeof Copy>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof HeadlineText> = (args) => <HeadlineText {...args} />;
+const Template: ComponentStory<typeof Copy> = (args) => <Copy {...args} />;
 
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args

a6a7b7a79dac606369f6ee10fbee8df6d36bea60
Ove Doll <ove@ovedoll.de>
2022-09-01 20:04:51 +0200

    fix(storybook): fixes storybook build issue

--- packages/site/.storybook/main.js
+++ packages/site/.storybook/main.js
@@ -12,5 +12,13 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials'
   ],
-  framework: '@storybook/react'
+  framework: '@storybook/react',
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    })
+    return config
+  }
 };
